cbcbd8bbf53b9a3ca7f22ba801a8a5372b3842e2
PromotezCitizen <gkstkdgus821@gmail.com>
2022-11-04 11:43:21 +0900

    맵 제작 및 맵 탈줄 방지 처리 완료

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    float h, v;
+    Rigidbody2D rigid;
+
+    private void Awake() {
+        rigid = GetComponent<Rigidbody2D>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+
+        transform.Translate(new Vector2(h, v) * Time.deltaTime * speed); // 
+    }
+
+/*     private void FixedUpdate() {
+        rigid.velocity = new Vector2(h, v) * speed;    
+    } */
+}
