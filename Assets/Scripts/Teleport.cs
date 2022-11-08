85ee45ef27f3bae26ecadd0cbb76ff6bdd2f8a5b
PromotezCitizen <gkstkdgus821@gmail.com>
2022-11-08 20:41:37 +0900

    * 포탈 이동 방식 최적화 - 소스코드 수정

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -10,17 +10,20 @@ public class Teleport : MonoBehaviour
     private float moveFactor = 0.1f;
 
     private float[] axis = new float[2];
+    private float[] align = new float[2];
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.tag == "Player") {
             axis[0] = Input.GetAxisRaw("Horizontal");
             axis[1] = Input.GetAxisRaw("Vertical");
 
-            axis[0] = axis[0] < 0 ? axis[0] - moveFactor : axis[0] + moveFactor;
-            axis[1] = axis[1] < 0 ? axis[1] - moveFactor : axis[1] + moveFactor;
+            align[0] = this.transform.position.x - player.transform.position.x;
+            align[1] = this.transform.position.y - player.transform.position.y;
+
+            axis[0] = axis[0] == 0 ? axis[0] - align[0] : axis[0] + align[0];
+            axis[1] = axis[1] == 0 ? axis[1] - align[1] : (axis[1] < 0 ? axis[1] + align[1] - moveFactor : axis[1] + align[1] + moveFactor);
 
             player.transform.position = new Vector2(portal.transform.position.x + axis[0], portal.transform.position.y + axis[1]);
-            // player.transform.position = portal.transform.position;
             print("enter!");
         }
     }
