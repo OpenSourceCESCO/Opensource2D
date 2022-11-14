1d422b88e80cdd84f62f5c299524185d8daed5c4
doodaad <junwoo9974@gmail.com>
2022-11-15 04:52:56 +0900

    +NPC와의 대화 파트를 보강(NPC별로 다른 대화, 많은 분량의 대화 가능)

---
+++ Assets/Scripts/TalkManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkManager : MonoBehaviour
+{
+    Dictionary<int, string[]> talkData;
+
+    void Awake()
+    {
+        talkData = new Dictionary<int, string[]>();
+        GenerateData();
+    }
+    void GenerateData()
+    {
+        talkData.Add(1, new string[] { "안녕?", "이 곳에 처음 왔구나?" });
+        talkData.Add(2, new string[] { "정체 모를 무언가.", "개발자 녀석이 테스트를 위해 배치한 듯 하다." });
+    }
+    public string GetTalk(int id, int talkIndex)
+    {
+        if (talkIndex == talkData[id].Length)
+            return null;
+        else
+            return talkData[id][talkIndex];
+    }
+}
