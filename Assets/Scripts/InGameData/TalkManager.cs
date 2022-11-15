c0a948ffc6a1e87943230beb402bb0726998353f
PromotezCitizen <gkstkdgus821@gmail.com>
2022-11-15 22:24:59 +0900

    + 대화 스크립트를 분리. 대화 스크립트 json 파일 생성 ^ 대화 스크립트를 분리함에 따라 대화 스크립트 json을 불러오는 코드 추가

--- Assets/Scripts/InGameData/TalkManager.cs
+++ Assets/Scripts/InGameData/TalkManager.cs
@@ -1,10 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System;
 
 public class TalkManager : MonoBehaviour
 {
     Dictionary<int, string[]> talkData;
+    TextDatas textDatas;
+
+    [Serializable]
+    public class TextData {
+        public int id;
+        public string[] text;
+        public void printDatas() {
+            Debug.Log(string.Format("{0} {1}", id, text[0]));
+        }
+    }
+
+    class TextDatas {
+        public TextData[] data;
+    }
 
     void Awake()
     {
@@ -13,9 +29,16 @@ public class TalkManager : MonoBehaviour
     }
     void GenerateData()
     {
-        talkData.Add(1, new string[] { "안녕?", "이 곳에 처음 왔구나?" });
-        talkData.Add(2, new string[] { "정체 모를 무언가.", "개발자 녀석이 테스트를 위해 배치한 듯 하다." });
-        talkData.Add(3, new string[] { "과제는 다했니?", "연구실에 관심있다면 오거라." });
+        string filepath = "Assets/Resources/Json/talkMessage.json";
+        string json = File.ReadAllText(filepath);
+        textDatas = JsonUtility.FromJson<TextDatas>(json);
+        
+        for (int i = 0; i < textDatas.data.Length; i++) {
+            talkData.Add(textDatas.data[i].id, textDatas.data[i].text);
+        }
+        // talkData.Add(1, new string[] { "안녕?", "이 곳에 처음 왔구나?" });
+        // talkData.Add(2, new string[] { "정체 모를 무언가.", "개발자 녀석이 테스트를 위해 배치한 듯 하다." });
+        // talkData.Add(3, new string[] { "과제는 다했니?", "연구실에 관심있다면 오거라." });
     }
     public string GetTalk(int id, int talkIndex)
     {
