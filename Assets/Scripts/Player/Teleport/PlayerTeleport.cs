dc7242f7932307571afb2c8201fa55c91c230e73
PromotezCitizen <gkstkdgus821@gmail.com>
2022-11-15 15:27:34 +0900

    ^ 플레이어가 포탈 이용시 현 위치로 이동하도록 수정. 좀 더 다듬어야 함

--- Assets/Scripts/Player/Teleport/PlayerTeleport.cs
+++ Assets/Scripts/Player/Teleport/PlayerTeleport.cs
@@ -2,43 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Teleport : MonoBehaviour
+public class PlayerTeleport : MonoBehaviour
 {
-/*     private GameObject player; */
-    public GameObject portal;
-
-    public Transform GetDestination() {
-        return portal.transform;
-    }
-    
-    
-    /*     
-    private float teleportTime;
-    private float moveFactor = 0.1f;
-
-    private float[] axis = new float[2];
-    private float[] align = new float[2];
-
     private GameObject currentTeleporter;
-
-    // Start is called before the first frame update
     void Start() {
-        player = GameObject.FindWithTag("Player");
-        teleportTime = this.transform.parent.GetComponent<TeleportParent>().getTeleportTime();
-        // print(teleportTime);
+        currentTeleporter = null;
     }
-
+    // Start is called before the first frame update
+    void Update() {
+        if (currentTeleporter != null) {
+            transform.position = currentTeleporter.GetComponent<Teleport>().GetDestination().position;
+        }
+    }
     private void OnTriggerExit2D(Collider2D collision) {
         if (collision.CompareTag("Portal")) {
-            currentTeleporter = collision.gameObject;
+            if (currentTeleporter != null) {
+                StartCoroutine(WaitForIt());
+                print("Exited!");
+            }
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.CompareTag("Portal")) {
-            if (collision.gameObject == currentTeleporter) {
-                currentTeleporter = null;
-                player.transform.position = new Vector2(portal.transform.position.x, portal.transform.position.y);
+            if (collision.gameObject != null && currentTeleporter == null) {
+                currentTeleporter = collision.gameObject;
+                print("entered!");
+                // player.transform.position = new Vector2(portal.transform.position.x, portal.transform.position.y);
             }
         }
         // if (collision.tag == "Player") {
@@ -55,6 +45,10 @@ public class Teleport : MonoBehaviour
 
         //     Timer.rTime -= teleportTime; // 텔레포트 이동 시간. 타이머의 변수에 직접 적용
         // }
-    } */
+    }
 
+    IEnumerator WaitForIt() {
+        yield return new WaitForSeconds(1);
+        currentTeleporter = null;
+    }
 }
