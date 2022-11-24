4399391664b3212ae303b159658b6f92cc850eeb
PromotezCitizen <gkstkdgus821@gmail.com>
2022-11-24 11:12:12 +0900

    ^ 남은 행동력 회전하여 정렬은 슬라이더 이미지 오류로 인해 임시 보류

--- Assets/Scripts/Player/LeftMovement/PlayerLeftMovements.cs
+++ Assets/Scripts/Player/LeftMovement/PlayerLeftMovements.cs
@@ -11,29 +11,34 @@ public class PlayerLeftMovements : MonoBehaviour
     float sliderFactor = 0.08333f;
     int initMoveValue = 6;
     public int additionalMoveLeft = 4;
-    public int moveLeft = 5;
-    
+    public int moveLeft = 2;
+
     // Start is called before the first frame update
     void Start()
     {
+        print(moveLeft);
         player = GameObject.FindWithTag("Player");
         move = this.transform.Find("Move").gameObject.GetComponent<Slider>();
         additionalMove = this.transform.Find("AdditionalMove").gameObject.GetComponent<Slider>();
 
         move.value = sliderFactor * moveLeft;
         additionalMove.value = sliderFactor * additionalMoveLeft;
-        if (moveLeft < 6) {
+        // 회전 시 모양이 이상하게 변하여 해결할때 까지는 임시보류
+/*         if (moveLeft < initMoveValue)
+        { // 기본 행동력이 까임에 따라 추가 행동력의 위치 변화
             additionalMove.transform.Rotate(new Vector3(0, 0, 30 * (initMoveValue - moveLeft)));
-        }
+        } */
     }
 
     // Update is called once per frame
     void Update()
     {
+        // 플레이어의 위치를 따라가도록 설정
         this.transform.position = player.transform.position;
     }
 
-    public void ReduceSlider() {
+    public void ReduceSlider()
+    {
         if (additionalMoveLeft > 0) additionalMove.value = sliderFactor * --additionalMoveLeft;
         else if (moveLeft > 0) move.value = sliderFactor * --moveLeft;
     }
